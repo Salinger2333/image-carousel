63ea2eb3a603251cf2179c4806c462b27d64e18c
Salinger2333 <1565664814@qq.com>
2025-12-02 18:11:51 +0800

    feature:add 2 buttons to slide

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log("Hello from index.js!");
+import "./style.css";
+const imgList = document.querySelectorAll(".img-slide");
+imgList.forEach((img, index) => {
+  img.dataset.index = index;
+});
+active();
+const pre = document.querySelector(".pre");
+const next = document.querySelector(".next");
+
+const changeCarsouel = (function(){
+  let curIndex = 0;
+  const length = imgList.length;
+  const increase = () => {
+    if (curIndex < length - 1) {
+      curIndex++;
+    } else if (curIndex === length - 1) {
+      curIndex = 0;
+    }
+    active(curIndex);
+  };
+  const decrease = () => {
+    if (curIndex > 0) {
+      curIndex--;
+    } else if (curIndex === 0) {
+      curIndex = length - 1;
+    }
+    active(curIndex);
+  };
+  const changeByIndex = (index) => {
+    curIndex = index
+    active(index)
+  }
+
+  return {
+    increase,
+    decrease,
+    changeByIndex
+  };
+})();
+
+function active(editIndex = 0) {
+  imgList.forEach((img, index) => {
+    img.classList.remove("is-active");
+    if (index == editIndex) {
+      img.classList.add("is-active");
+    }
+  });
+}
+pre.addEventListener("click", () => {
+  changeCarsouel.decrease()
+});
+next.addEventListener("click", () => {
+  changeCarsouel.increase()
+});
+
